7f6e4f5235de53aa124942af08ff0ef429cd9065
Alicja Latała <alicja.latala@gmail.com>
2021-06-06 11:14:46 +0200

    Calculate trip price, add OrderForm, OrderSummary

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -1,23 +1,26 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import styles from './OrderForm.scss';
 
 import {Row, Col} from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary';
 
-const OrderForm = () => {
+const OrderForm = ({ tripCost, options }) => {
+  console.log('options', options);
   return (
     <div className={styles.component}>
       <Row>
         <Col xs={12}>
-          <OrderSummary />
+          <OrderSummary tripCost={tripCost} options={options}/>
         </Col>
       </Row>
     </div>
   );
 };
 
-// OrderForm.propTypes = {
-// };
+OrderForm.propTypes = {
+  tripCost: PropTypes.string,
+  options: PropTypes.object,
+};
 
 export default OrderForm;
